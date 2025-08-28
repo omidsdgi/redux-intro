8f5f27db4dd3e2e88da55301fd9c03f458682497
omidsdgi <omid69sdgi@gmail.com>
2025-08-28 08:46:42 +0330

    feat(store): set up Redux store with basic configuration

--- src/store/Store.tsx
+++ src/store/Store.tsx
@@ -1,7 +1,14 @@
-const initialState = {
+import {createStore} from "redux";
+interface initialStateType {
+    balance:number,
+    loan:number,
+    loanPurpose:string,
+}
+
+const initialState:initialStateType = {
     balance:0,
     loan:0,
-    loanPurpose:0,
+    loanPurpose:"",
 }
 
 function reducer(state = initialState, action) {
@@ -20,7 +27,9 @@ function reducer(state = initialState, action) {
                     if (state.loan>0) return state;
                     return {
                         ...state,
-                        loan: action.payload,
+                        loan: action.payload.amount,
+                        loanPurpose:action.payload.purpose,
+                        balance:state.balance + action.payload.amount,
                     }
                     case 'account/payLoan':
                         return {
@@ -33,3 +42,10 @@ function reducer(state = initialState, action) {
                             return state;
     }
 }
+export const store=createStore(reducer);
+
+store.dispatch({type:'account/deposit',payload:500});
+store.dispatch({type:'account/withdraw',payload:200});
+store.dispatch({type:'account/requestLoan',payload: {amount:1000,purpose:"buy a car"} });
+store.dispatch({type:'account/payLoan',payload:1000});
+console.log(store.getState());
