2c9a74546cc02679be70401e17056c75ff0b0d5f
omidsdgi <omid69sdgi@gmail.com>
2025-08-29 11:35:08 +0330

    setup dispatching actions from React app

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,15 +2,22 @@ import CreateCustomer from "@/features/customers/CreateCustomer";
 import Customer from "@/features/customers/Customer";
 import AccountOperations from "@/features/accounts/AccountOperations";
 import BalanceDisplay from "@/features/accounts/BalanceDisplay";
+import {useSelector} from "react-redux";
 
 export default function Home() {
+    const fullName = useSelector((state)=>state.customer.fullName);
   return (
       <div>
         <h1>🏦 The React-Redux Bank ⚛️</h1>
-        <CreateCustomer />
-        <Customer />
+          {fullName==="" ?(
+        <CreateCustomer/>
+              ):(
+              <>
+        <Customer/>
         <AccountOperations />
         <BalanceDisplay />
+              </>
+              )}
       </div>
   );
 }
