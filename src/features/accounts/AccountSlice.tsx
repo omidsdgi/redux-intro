fab91cef1d225badf8545ffea3580bcb8b668bb0
omidsdgi <omid69sdgi@gmail.com>
2025-08-30 10:39:00 +0330

    creating the account slice with React toolkit

--- src/features/accounts/AccountSlice.tsx
+++ src/features/accounts/AccountSlice.tsx
@@ -1,21 +1,51 @@
-import {Dispatch} from "react";
+import { createSlice} from "@reduxjs/toolkit";
 
-interface initialStateAccountType {
-    balance: number,
-    loan: number,
-    loanPurpose: string,
-    isLoading:boolean
-}
 
-interface Action {
-    type: string;
-    payload?: any;
-}
-const initialStateAccount: initialStateAccountType = {
+const initialState = {
     balance: 0,
     loan: 0,
     loanPurpose: "",
     isLoading:false
+}
+
+const accountSlice = createSlice({
+    name:"account",
+    initialState,
+    reducers:{
+        deposit(state, action){
+            state.balance += action.payload;
+        },
+        withdraw(state, action){
+            state.balance -= action.payload;
+        },
+        requestLoan:{
+            prepare(amount,purpose){
+                return {
+                    payload: {amount,purpose}
+                }
+            },
+            reducer (state, action){
+            if (state.loan>0) return;
+           state.loan=action.payload.amount;
+           state.loanPurpose = action.payload;
+           state.balance += action.payload;
+        }},
+        payLoan(state, action){
+            state.balance -= state.loan;
+            state.loan=0;
+            state.loanPurpose = "";
+        }
+    }
+})
+console.log(accountSlice)
+export const {deposit,withdraw,requestLoan,payLoan}=accountSlice.actions;
+
+export default accountSlice.reducer;
+
+
+/*interface Action {
+    type: string;
+    payload?: any;
 }
 export default function accountReducer(state = initialStateAccount, action: Action) {
     switch (action.type) {
@@ -80,4 +110,4 @@ export function requestLoan(amount: number, loanPurpose: string) {
 
 export function payLoan(amount: number) {
     return { type: 'account/payLoan',payload: amount }
-}
+}*/
